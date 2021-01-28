6490fc72b81d7c82c4482ca52e510b964b3f263b
“ViktorOsa” <viktor.osadchyi1@gmail.com>
2021-01-28 21:43:37 +0100

    added signup/login

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,4 +1,17 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+
+    //Note: we don't have PostMapping(), because we handle login process inside AuthenticationService
+
+    @GetMapping()
+    public String loginView(){
+        return "login";
+    }
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AuthenticationService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AuthenticationService.java
@@ -1,8 +1,45 @@
 package com.udacity.jwdnd.course1.cloudstorage.service;
 
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-@Service
-public class AuthenticationService {
+import java.util.ArrayList;
 
+@Service
+public class AuthenticationService implements AuthenticationProvider {
+
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    public AuthenticationService(UserMapper userMapper, HashService hashService){
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication){
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        //check credentials
+        User user = userMapper.getUser(username);
+        if(user != null){
+            String encodedSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+            if(user.getPassword().equals(hashedPassword)){
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass){
+        return aClass.equals(UsernamePasswordAuthenticationToken.class);
+    }
 }
